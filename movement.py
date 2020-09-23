fe8ce15e1a597cd181b049ede0900a2779492316
Mark Snyder <marksnyder2101@flpoly.org>
2020-09-23 12:12:39 -0400

    Add files via upload

--- movement.py
+++ movement.py
@@ -8,11 +8,7 @@ from sensor_msgs.msg import LaserScan
 pose = 0.0
 
 class laserread:
-        angleIncrement = 0
-        desiredIndex = 0
-        forwardIndex = 0
-        currentRange = 1.0
-        maxRan = 8.0
+        
         def __init__(self):
                 self.FirstValue=0
                 self.maxRan = 8.0
@@ -28,6 +24,7 @@ class laserread:
 
         def callback(self, data):
             #rospy.loginfo(rospy.get_caller_id() + "The First Value is " + str(data.ranges[FirstValue]))
+            print "LaserRead class, callback() function being called"
             forwardIndex = int(len(data.ranges)/2)
             maxRan = data.range_max
             currentRange = data.ranges[forwardIndex]
@@ -39,26 +36,37 @@ class laserread:
             
 def odomcallback(odom_data):
     #start callback for the odometer
+    global pose 
     pose = odom_data.pose.pose.position.x
     #print "The odometer is at x = " + str(odom_data.pose.pose.position.x)
 
 def go_forward():
+        global pose
 	pub = rospy.Publisher("/robot0/cmd_vel", Twist, queue_size = 10)
         rospy.Subscriber("/robot0/odom", Odometry, odomcallback)
-        laserread()
+        myL = laserread()
 	rospy.init_node("Controller", anonymous=True)
 	rate = rospy.Rate(10)
         twistCmd = Twist();
-        
+        print "Laser reader class variable maxRan is: " + str(myL.maxRan)
 	while pose < 4:
                 #print "globabl variable pose is: " + str(pose)
-                turn = laserread.desiredIndex - laserread.forwardIndex
-                twistCmd.linear.x = laserread.currentRange / laserread.maxRan
-		twistCmd.angular.z = turn * laserread.angleIncrement
+                
+                twistCmd.linear.x = myL.currentRange / myL.maxRan
+		twistCmd.angular.z = 0
 		pub.publish(twistCmd)
 		rate.sleep()
-       
+
+        twistCmd.linear.x = 0
+        twistCmd.angular.z = 0
+        pub.publish(twistCmd)
+        rate.sleep()
         
+        turn = myL.desiredIndex - myL.forwardIndex
+        twistCmd.linear.x = myL.currentRange / myL.maxRan
+        twistCmd.angular.z = turn * myL.angleIncrement
+        pub.publish(twistCmd)
+        rate.sleep()
 
 if __name__ == '__main__':
 	try:
